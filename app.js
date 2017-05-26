759d738fcb9698a08506792a84e8abade6689c08
Destanek <enigmadoublem@gmail.com>
2017-05-26 10:41:37 +0900

    read json file and add mysql lib

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ var express = require('express'),
    	app = express(),
 	bodyParser = require('body-parser'),
 	path = require("path");
+var mysql = require('mysql');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'www')));
+
+app.post('/', function(req, res){
+	var device = req.body.Device;
+	var code = req.body.Code;
+	var time = req.body.Time;
+	var home = req.body.Home;
+	console.log("Device : " + device + ", Code : " + code + ", Time :" + time + ", Home : " + home);
+
+	fs.readFile('data.json', 'utf8', function(err, data) {
+		if(err) throw err;
+		obj = JSON.parse(data);
+		console.log(obj.data[device][code]);
+		res.setHeader('Content-type', 'text/html');
+		res.send("Message : " + obj.data[device][code]);
+	});
+
+	//insertData(device, code, time, home);
+});
 
 var server = http.createServer(function(request, response) {
 
