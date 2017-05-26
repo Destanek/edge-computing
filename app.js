8e8a36e2e53b1714bc031ae2a1f66ae82a30f04f
Destanek <enigmadoublem@gmail.com>
2017-05-26 10:36:27 +0900

    minor change with more express lib

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 var express = require('express'),
-    app = express(),
+   	app = express(),
 	bodyParser = require('body-parser'),
 	path = require("path");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'www')));
 
 var server = http.createServer(function(request, response) {
 
