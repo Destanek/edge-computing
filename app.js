193a62f10bf46d4c0d96ce9eb7a2659900375da1
Destanek <enigmadoublem@gmail.com>
2017-05-26 10:23:17 +0900

    test simple page

--- app.js
+++ app.js
@@ -1,52 +1,13 @@
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	path = require("path");
-
 var http = require('http');
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-   host: "kiban.database.windows.net",
-   user: "charattoon",
-   password: "Steal1412",
-   database: "edge"
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Azure!");
+
 });
 
-var fs = require('fs');
-var obj;
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'www')));
-
-app.post('/', function(req, res){
-	var device = req.body.Device;
-	var code = req.body.Code;
-	var time = req.body.Time;
-	var home = req.body.Home;
-	console.log("Device : " + device + ", Code : " + code + ", Time :" + time + ", Home : " + home);
-
-	fs.readFile('data.json', 'utf8', function(err, data) {
-		if(err) throw err;
-		obj = JSON.parse(data);
-		console.log(obj.data[device][code]);
-		res.setHeader('Content-type', 'text/html');
-		res.send("Message : " + obj.data[device][code]);
-	});
-
-	insertData(device, code, time, home);
-});
-
-function insertData(device, code, time, home){
-	var sql = "INSERT INTO MainLog (Device, Code, Time, Home) VALUES ('" + device + "','" + code + "','" + time + "','" + home + "')";
-	con.query(sql, function(err, result){
-		if(err) throw err;
-	});
-}
-
-app.listen();
-//var server = app.listen(8081, function () {
-//   var port = server.address().port; 
-//   console.log("Server is running on port %s", port);
-//});
+console.log("Server running at http://localhost:%d", port);
